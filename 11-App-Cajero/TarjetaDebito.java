0d81abda4f4791fbc7b33c383a51d9ade7abcb3d
lNagatoro <ag016498@gmail.com>
2023-03-30 08:28:26 -0500

    Cajero

---
+++ 11-App-Cajero/TarjetaDebito.java
@@ -0,0 +1,64 @@
+
+public class TarjetaDebito{
+
+	// Atributos
+	private String numero;
+	private String titular;
+	private String password;
+	private int saldo;
+	
+
+
+	// Metodos
+	public TarjetaDebito(String numero, String titular, String password, int saldo){
+		this.numero = numero;
+		this.titular = titular;
+		this.password = password;
+		this.saldo = saldo;
+		
+	}
+
+	public boolean disminuirSaldo(int valor){
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			// registrar en log de tarjeta
+			return true;
+		}else{
+			// registrar en log de la tarjeta
+			return false;
+		}
+	}
+
+	public int getSaldo(){
+		return this.saldo;
+	}
+
+	public String getNumero(){
+		return this.numero;
+	}
+	
+	public String getpassword(){
+		return this.password;
+	}
+
+	public void setpassword(){
+		this.password = password;
+	}
+
+
+	public boolean verificarPassword(String password){
+		return this.password.equals(password);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
