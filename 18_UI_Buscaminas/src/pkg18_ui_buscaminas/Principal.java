6cec5fbd1b81ad01a67b2e9b9271ff11bc7a8091
Miguel-Alzate <miguelangel4817@gmail.com>
2023-05-11 10:21:19 -0500

    Nuevos

---
+++ 18_UI_Buscaminas/src/pkg18_ui_buscaminas/Principal.java
@@ -0,0 +1,9 @@
+package pkg18_ui_buscaminas;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        
+        UI_Buscaminas ventana_01 = new UI_Buscaminas();
+    }  
+}
---
+++ JUEGO_TRIKI/Principal.java
@@ -0,0 +1,7 @@
+public class Principal{
+    public static void main (String[] args){
+
+        Triki evento_01 = new Triki();
+        
+    }
+}
